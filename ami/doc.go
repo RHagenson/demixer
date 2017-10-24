dcc1ca0470a1faae0700785d86d4b4859aaa9d0a
Ryan Hagenson <Ryan.Hagenson@gmail.com>
2017-10-24 12:06:01 -0500

    Added ami doc file

---
+++ ami/doc.go
@@ -0,0 +1,9 @@
+/*
+Package ami computes the Average Mututal Information (AMI) Profile for a given
+sequence. There will also eventually be utilities to compute the similarity
+between two AMI Profiles via one minus the correlation coefficient and compute
+a matrix of similarities when given a collection of AMI Profiles. There
+should also be a utility that accepts AMI Profiles on a channel and computes
+similarities as Profiles are generated.
+*/
+package ami
