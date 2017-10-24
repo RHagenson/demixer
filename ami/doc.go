7d895c3284fc5651f348335652c6afaa0de4468b
Ryan Hagenson <Ryan.Hagenson@gmail.com>
2017-10-24 12:08:57 -0500

    Added reference article

--- ami/doc.go
+++ ami/doc.go
@@ -5,5 +5,8 @@ between two AMI Profiles via one minus the correlation coefficient and compute
 a matrix of similarities when given a collection of AMI Profiles. There
 should also be a utility that accepts AMI Profiles on a channel and computes
 similarities as Profiles are generated.
+
+This methodology is derived from:
+  Bauer, et al. 2008. https://doi.org/10.1186/1471-2105-9-48
 */
 package ami
