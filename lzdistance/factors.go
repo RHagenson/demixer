8dd9b496843b84e9ad7cf5cb1cb32c29b41fc564
Ryan Hagenson <Ryan.Hagenson@gmail.com>
2017-10-26 16:29:29 -0500

    Use LZ lengths directly and test for subadditivity

--- lzdistance/factors.go
+++ lzdistance/factors.go
@@ -2,13 +2,24 @@ package lzdistance
 
 import "bitbucket.org/rhagenson/demixer/dna"
 
-// LZFactors takes two Sequences and computes the four possible
+// LZ76Factors takes two Sequences and computes the four possible
 // factorizations c(S), c(Q), c(S,Q), and c(Q,S)
-func LZFactors(s, q dna.Sequence) (cs, cq, csq, cqs int) {
-	cs = Eh(s).Length()
-	cq = Eh(q).Length()
-	csq = Eh(s.AppendSeq(q)).Length()
-	cqs = Eh(q.AppendSeq(s)).Length()
+func LZ76Factors(s, q dna.Sequence) (cs, cq, csq, cqs int) {
+	cs = LZ76Length(s)
+	cq = LZ76Length(q)
+	csq = LZ76Length(s.AppendSeq(q))
+	cqs = LZ76Length(q.AppendSeq(s))
+
+	return
+}
+
+// LZ78Factors takes two Sequences and computes the four possible
+// factorizations c(S), c(Q), c(S,Q), and c(Q,S)
+func LZ78Factors(s, q dna.Sequence) (cs, cq, csq, cqs int) {
+	cs = LZ78Length(s)
+	cq = LZ78Length(q)
+	csq = LZ78Length(s.AppendSeq(q))
+	cqs = LZ78Length(q.AppendSeq(s))
 
 	return
 }
---
+++ lzdistance/factors_test.go
@@ -0,0 +1,67 @@
+package lzdistance
+
+import (
+	"math"
+	"math/rand"
+	"testing"
+
+	"bitbucket.org/rhagenson/demixer/dna"
+)
+
+// TestZ76FactorsHaveSubadditivity checks that the produced factors hold to:
+//    1. c(SQ) - c(S) <= c(Q)
+//    2. c(QS) - c(Q) <- c(S)
+func TestLZ76FactorsHaveSubadditivity(t *testing.T) {
+	// Generate two sequences at random in parallel
+	seqchan := make(chan dna.Sequence, 2)
+	go func(seq chan dna.Sequence) {
+		seq <- randSeq(rand.Intn(math.MaxInt16))
+	}(seqchan)
+	go func(seq chan dna.Sequence) {
+		seq <- randSeq(rand.Intn(math.MaxInt16))
+	}(seqchan)
+
+	// Gather the sequences
+	s := <-seqchan
+	q := <-seqchan
+
+	// Generate factors
+	cs, cq, csq, cqs := LZ76Factors(s, q)
+
+	// Check c(S,Q) - c(S) <= c(Q)
+	if !((csq - cs) <= cq) {
+		t.Errorf("Expected (%#v - %#v) <= %#v however this was not true.", csq, cs, cq)
+	}
+	if !((cqs - cq) <= cs) {
+		t.Errorf("Expected (%#v - %#v) <= %#v however this was not true.", cqs, cq, cs)
+	}
+}
+
+// TestZ76FactorsHaveSubadditivity checks that the produced factors hold to:
+//    1. c(SQ) - c(S) <= c(Q)
+//    2. c(QS) - c(Q) <- c(S)
+func TestLZ78FactorsHaveSubadditivity(t *testing.T) {
+	// Generate two sequences at random in parallel
+	seqchan := make(chan dna.Sequence, 2)
+	go func(seq chan dna.Sequence) {
+		seq <- randSeq(rand.Intn(math.MaxInt16))
+	}(seqchan)
+	go func(seq chan dna.Sequence) {
+		seq <- randSeq(rand.Intn(math.MaxInt16))
+	}(seqchan)
+
+	// Gather the sequences
+	s := <-seqchan
+	q := <-seqchan
+
+	// Generate factors
+	cs, cq, csq, cqs := LZ78Factors(s, q)
+
+	// Check c(S,Q) - c(S) <= c(Q)
+	if !((csq - cs) <= cq) {
+		t.Errorf("Expected (%#v - %#v) <= %#v however this was not true.", csq, cs, cq)
+	}
+	if !((cqs - cq) <= cs) {
+		t.Errorf("Expected (%#v - %#v) <= %#v however this was not true.", cqs, cq, cs)
+	}
+}
